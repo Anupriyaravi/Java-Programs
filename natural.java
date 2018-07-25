cb8ec4e06e3b5e471ed67cdd9ade4621a9bb7c57
Anupriyaravi <41136677+Anupriyaravi@users.noreply.github.com>
2018-07-25 12:00:28 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class natural {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	int sum=0;
+	for(int i=0;i<=n;i++){
+		sum=sum+i;
+	}
+	System.out.println(sum);
+	}
+
+}
