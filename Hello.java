8b4b08381c74b9ca30f4c838c1ade84df3c4e771
Anupriyaravi <41136677+Anupriyaravi@users.noreply.github.com>
2018-07-25 11:43:11 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+int n=in.nextInt();
+	for(int i=1;i<=n;i++)
+		System.out.println("Hello");
+	}
+
+}
