23489ba286f6245da6118e91349c2d28893e2732
Anupriyaravi <41136677+Anupriyaravi@users.noreply.github.com>
2018-07-25 18:00:27 +0530

    Create mulfive.java

---
+++ mulfive.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class mulfive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		for(int i=1;i<=n;i++){
+	System.out.println(n*i);
+}
+	}
+
+}
