72f5586a94a3f65de989848c72fab49dcfda3ec1
Anupriyaravi <41136677+Anupriyaravi@users.noreply.github.com>
2018-07-28 15:24:05 +0530

    Create space.java

---
+++ space.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class space {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner in=new Scanner(System.in);
+	String s=in.nextLine();
+	String str[]=s.split(" ");
+	int l=str.length;
+	System.out.println(l-1);
+	}
+
+}
