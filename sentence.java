8ab63cb8833b765b86567b1da48bd4d2b6567081
Anupriyaravi <41136677+Anupriyaravi@users.noreply.github.com>
2018-07-28 16:21:37 +0530

    Update sentence.java

--- sentence.java
+++ sentence.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 import java.util.StringTokenizer;
 public class armstronginterval {
-							public static void main(String[] args) {
-		  Scanner in=new Scanner(System.in);
+		public static void main(String[] args) {
+		 Scanner in=new Scanner(System.in);
 		String s=in.nextLine();
 		StringTokenizer st = new StringTokenizer(s,".");
 		System.out.println(st.countTokens());
