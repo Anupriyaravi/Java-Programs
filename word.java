b1051f870846d1bc26c99c34badc6c2282eb42d3
Anupriyaravi <41136677+Anupriyaravi@users.noreply.github.com>
2018-07-28 13:25:15 +0530

    Create word.java

---
+++ word.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class words {
+	public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+String s=in.nextLine();
+String str[]=s.split(" ");
+System.out.println(str.length);
+	}
+
+}
