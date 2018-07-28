18eab289e6abb7a40714eede0cce1a20c1ff95c6
Anupriyaravi <41136677+Anupriyaravi@users.noreply.github.com>
2018-07-28 13:16:55 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class count {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner in=new Scanner(System.in);
+String s=in.nextLine();
+String s1=s.replaceAll(" ","");
+int l=s1.length();
+System.out.println(l);
+	}
+
+}
