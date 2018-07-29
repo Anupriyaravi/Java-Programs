4e196421e1c0f23a0de205c2a6885f73f4691012
Anupriyaravi <41136677+Anupriyaravi@users.noreply.github.com>
2018-07-29 15:45:25 +0530

    Create stringreverse.java

---
+++ stringreverse.java
@@ -0,0 +1,10 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s="anupriya";
+		String q=new StringBuffer(s).reverse().toString();
+		System.out.println(q);
+	}
+}
